1dbe32820723650d7a8caf6a734597342c442e54
Nate Allen <nate.allen@10up.com>
2018-11-08 16:28:29 -0500

    Add links to nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
     render: function(){
         return (
-            <h2>Navigation Component</h2>
+            <div>
+                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </div>
         )
     }
 });
