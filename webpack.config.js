eba4b47d9a54603b0e8335c60864e22e39538d19
Nate Allen <nate.allen@10up.com>
2018-11-08 14:27:49 -0500

    Add Main and Nav aliases

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
     },
     resolve: {
         root: __dirname,
-        alias: {},
+        alias: {
+            Main: 'app/components/Main.jsx',
+            Nav: 'app/components/Nav.jsx'
+        },
         extensions: ['', '.js', '.jsx']
     },
     module: {
