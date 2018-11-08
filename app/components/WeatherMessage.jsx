ab6081785b4bcbb2dc4621e55912e6dbbe7bd58a
Nate Allen <nate.allen@10up.com>
2018-11-08 17:25:56 -0500

    Add WeatherMessage component

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+    render: function() {
+        return (
+            <h3>It's 45 in Boston</h3>
+        )
+    }
+});
+
+module.exports = WeatherMessage;
