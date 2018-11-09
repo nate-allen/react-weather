ec59854cebbc94d9ca1aee376f6757b7e58ebbdc
Nate Allen <nate.allen@10up.com>
2018-11-09 10:12:27 -0500

    Display the weather and location

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
     render: function() {
+        var {temp, location} = this.props;
+
         return (
-            <h3>It's 45 in Boston</h3>
+            <h3>It's {temp}° in {location}</h3>
         )
     }
 });
