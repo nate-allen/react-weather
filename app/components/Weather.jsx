2decb1223c72444fa37a4361b37cc8f10fad760e
Nate Allen <nate.allen@10up.com>
2018-11-09 10:11:35 -0500

    Add handleSearch method

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,14 +1,46 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
+    getInitialState: function(){
+        return {
+            isLoading: false
+        }
+    },
+    handleSearch: function(location){
+        var self = this;
+
+        this.setState({isLoading: true});
+
+        openWeatherMap.getTemp(location).then(function(temp){
+            self.setState({
+                isLoading: false,
+                location: location,
+                temp: temp
+            });
+        }, function(errorMessage){
+            self.setState({isLoading: false});
+            alert(errorMessage);
+        });
+    },
     render: function() {
+        var {isLoading, temp, location} = this.state;
+
+        function renderMessage() {
+            if (isLoading) {
+                return <h3>Fetching weather...</h3>;
+            } else if (temp && location) {
+                return <WeatherMessage temp={temp} location={location}/>;
+            }
+        }
+
         return (
             <div>
                 <h1>Get Weather</h1>
-                <WeatherForm/>
-                <WeatherMessage/>
+                <WeatherForm onSearch={this.handleSearch}/>
+                {renderMessage()}
             </div>
         )
     }
